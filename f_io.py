85fa39ecb2f8a5e61b10486009c222a43c073350
Golan Trevize <98289178+SegFun01@users.noreply.github.com>
2022-08-21 12:06:47 -0600

    Iniciar f_io.py, falta

---
+++ f_io.py
@@ -0,0 +1,24 @@
+# f_var.py
+# Otras funciones varias
+import math
+
+def uso():
+   print("")
+   print("Modo de uso:  python mgh.py nombre_archivo.mgh opcion")
+   print("-----------------------------------------------------")
+   print( "")
+   print("Opciones:")
+   print("-n: modo normal, por defecto, imprime tablas de datos de entrada y salida")
+   print("")
+   print("-q: modo silencioso, solo imprime los vectores H y Q, de alturas piezométricas")
+   print("    en los nudos y caudales en los tramos")
+   print("")
+   print("-v: modo elocuente, imprime tablas de datos de entrada y salida, todos los vec-")
+   print("    tores y matrices; y los resultados de cada iteración")
+   print("")
+   print("Observaciones:")
+   print("La salida del programa va dirigida a la consola: \"stdout\".")
+   print("Para redirigir la salida use entubamiento con > o con >>")
+   print("")
+   print("Ejemplo: python mgh.py Ejemplo.mgh -v > Ejemplo.mgh.out")
+   print("")
