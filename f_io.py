56deb838c7144ed3632f606c867d9d5902a6b531
Golan Trevize <98289178+SegFun01@users.noreply.github.com>
2022-08-31 13:09:00 -0600

    se trabajó en las rutinas de impresión-> falta hacer pruebas

--- f_io.py
+++ f_io.py
@@ -34,8 +34,16 @@ def uso():
 #----------Fin de uso()
 
 #---------->>>>>>>>>> Función para imprimir los datos de salida en modo quiet
-def imprime_salida_quiet():
-   print("quiet: Imprime solo [Q] y [H] finales")
+def imprime_salida_quiet(Q,H,e,ns):      # Imprime caudales en los tramos y presiones en los nudos de demanda
+   print("  [Q] l/s")
+   for in in range(Q.size):
+      print(f"| {(Q[i]*1000):7.2f} |")
+   print("")   
+   print("  [P] m")
+   for in in range(H.size):
+      print(f"| {(H[i]-e[i+ns]):7.2f} |")
+   print("") 
+  
 #-----------
 
 #---------->>>>>>>>>> Función que imprime la salida normal: tablas de nudos y tramos
