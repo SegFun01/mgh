6e541a568eee5f62c0b0d6488d2d9c5a04b30c74
Carlos Camacho <98289178+SegFun01@users.noreply.github.com>
2022-09-21 13:24:46 -0600

    archivo con funcion para cargar curvas

--- curvas.py
+++ curvas.py
@@ -1,25 +1,18 @@
 ###### LEER LOS DATOS DEL LA RED DESDE UN ARCHIVO DE JSON 
 import json
 
-curva1={}
-curva2=[]
-def leer_json(c_id):
-    curva={}
-    with open('./projects/PruebaX.pat.json','r') as curvas:
+curva1=[]
+
+def get_curva(c_id):
+    curva=[]
+    with open('curvas.json','r') as curvas:
        j_curvas = json.load(curvas)
-
-    print(json.dumps(j_curvas, indent=4))
-    print("-----")
-
-    
-    for i in (j_curvas['curvas']): # leer los nudos de carga del JSON
-       if i.get('id')==c_id:
-         curva=i.get('curva')
+    curva=i.get(c_id)
     return curva
     
-x=int(input("Seleccione el id de la curva: "))
-curva2=leer_json(x)
-print("Curva 2")
-print(curva2)
-print(f"Un sdolo dato, en [5]: f={curva2[5]}")
+x=input("Seleccione el id de la curva: ")
+curva1=get_curva(x)
+print("Curva 1")
+print(curva1)
+print(f"Un sdolo dato, en [5]: f={curva1[5]}")
 
